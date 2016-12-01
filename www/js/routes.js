fb9a6c2d7214791035ad712de8b1e702683902b1
tsupol <ton.supol@gmail.com>
2016-12-01 15:27:11 +0700

    Initial commit

---
+++ www/js/routes.js
@@ -0,0 +1,43 @@
+angular.module('main')
+    .config(function ($stateProvider) {
+        $stateProvider
+
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html'
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/browse.html'
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+
+    })
+;
